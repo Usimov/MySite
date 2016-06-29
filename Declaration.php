9f91e83ccf583ce259a6534021d4bc2b80b6734f
Usimov <ilovevvp@tut.by>
2016-06-29 09:59:06 +0300

    Add existing file

--- Declaration.php
+++ Declaration.php
@@ -5,4 +5,4 @@
 
 <!-- <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> -->
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html>
