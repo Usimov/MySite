5876e7ca72454234e76827dfa89aea73ee0dd652
Usimov <ilovevvp@tut.by>
2016-06-29 10:37:56 +0300

    Add existing file

--- Declaration.php
+++ Declaration.php
@@ -3,6 +3,5 @@
     setlocale(LC_ALL,'ru_RU.65001','rus_RUS.65001','Russian_Russia.65001','russian');
 ?>
 
-<!-- <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd"> -->
 
 <!DOCTYPE html>
