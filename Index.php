1e35d350e873ab3997fd4ad9c9e54b77093ed283
Usimov <ilovevvp@tut.by>
2016-07-07 11:59:14 +0300

    Add existing file

--- Index.php
+++ Index.php
@@ -12,8 +12,8 @@
         require "Header.php";
     ?>
     <!-- Горизонтальная ориентация -->
-    <iframe src='inwidget/index.php?width=800&inline=7&view=14&toolbar=false'
-            scrolling='no' frameborder='no' style='border:none;width:800px;height:295px;overflow:hidden;'></iframe>
+    <!-- <iframe src='inwidget/index.php?width=800&inline=7&view=14&toolbar=false' -->
+            <!-- scrolling='no' frameborder='no' style='border:none;width:800px;height:295px;overflow:hidden;'></iframe> -->
 
     <div id="wrapper">
 	    <div class="section maxw">
